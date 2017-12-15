1e7dcdaf0ec2d95e4eba471f68f052d7b0323c1b
Victor <victor.parodi.montero@gmail.com>
2017-12-15 18:24:16 -0500

    master commit 2
    Instalando react-router, react-router-dom. Estableciendo las primera rutas de la aplicación.

---
+++ imports/ui/Link.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Link extends Component {
+    render() {
+        return (
+            <p> Link Component </p>
+        );
+    }
+}
+
+export default Link;
---
+++ imports/ui/Signup.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    render() {
+        return (
+            <h1> Signup component here! </h1>
+        );
+    }
+}
+
+export default Signup;
